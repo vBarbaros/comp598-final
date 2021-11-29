ae8d94e71ca8751527d6a0462cfb33618a6da0af
vBarbaros <victor.barbarosh@gmail.com>
2021-11-29 16:25:26 -0500

    Adapt the annotation handler script to account for third column

--- src/twitter_annotation_handler.py
+++ src/twitter_annotation_handler.py
@@ -6,7 +6,7 @@ import pandas as pd
 from twitter_collect import get_file_paths, read_from_csv_collected_tweets, append_to_csv
 
 SAMPLE_SIZE = 200
-ACCEPTED_COLUMNS = ['text', 'label', 'sentiment']
+ACCEPTED_COLUMNS = ['text', 'label', 'sentiment', 'ca_related']
 USE_FILTER = 3
 OUT_FILE_JSON, OUT_FILE_CSV, OUT_FILE_FILTERED_CSV, OUT_FILE_FILTERED_JSON, SEARCH_QUERY = get_file_paths(USE_FILTER)
 
@@ -28,33 +28,35 @@ def main():
             tmp_row = df_annotated_so_far.loc[df_annotated_so_far['text'] == row['text']]
             print(f"\nPREVIOUSLY LABELED")
             print('=====================================\n', tmp_row['text'].values[0], '\n=====================================')
-            print(f"label - [ {tmp_row['label'].values[0]} ] and sentiment - [ {tmp_row['sentiment'].values[0]} ]")
+            print(f"label - [ {tmp_row['label'].values[0]} ], sentiment - [ {tmp_row['sentiment'].values[0]} ], ca_related - [ {tmp_row['ca_related'].values[0]} ]")
             print('=====================================', '\n=====================================')
             rewrite = input('\nIf you want to rewrite labels enter \'z\' and hit ENTER: ')
             if rewrite == 'z':
                 print('=====================================\n', tmp_row['text'].values[0],
                       '\n=====================================\n')
-                label = input('Provide a Label for this tweet and hit ENTER: ')
-                sentiment = input('\nProvide a sentiment for this tweet and hit ENTER: ')
+                label = input('Provide a Label and hit ENTER: ')
+                sentiment = input('\nProvide a sentiment and hit ENTER: ')
+                ca_related = input('\nProvide a ca_related flag and hit ENTER: ')
                 df_annotated_so_far.at[tmp_row.index, 'label'] = label
                 df_annotated_so_far.at[tmp_row.index, 'sentiment'] = sentiment
+                df_annotated_so_far.at[tmp_row.index, 'ca_related'] = ca_related
                 print()
         else:
             print('\nTweed ID: ', row['tweet_id'])
             print('=====================================\n', row['text'], '\n=====================================\n')
-            label = input('Provide a Label for this tweet and hit ENTER: ')
-            sentiment = input('\nProvide a sentiment for this tweet and hit ENTER: ')
-
+            label = input('Provide a Label and hit ENTER: ')
+            sentiment = input('\nProvide a sentiment and hit ENTER: ')
+            ca_related = input('\nProvide a ca_related flag and hit ENTER: ')
 
             print(f"\n You just labeled the tweet")
             print('\n=====================================\n', row['text'], '\n=====================================')
-            print(f"label - [ {label} ] and sentiment - [ {sentiment} ]\n")
+            print(f"label - [ {label} ], sentiment - [ {sentiment} ], ca_related - [ {ca_related} ]\n")
             print(f"...saving and going to the next tweet...\n")
             print('\n=====================================', '\n=====================================')
 
-            df_annotated_data_list.append((row['text'], label, sentiment))
-        if i == 5:
-            break
+            df_annotated_data_list.append((row['text'], label, sentiment, ca_related))
+        # if i == 5:
+        #     break
 
     if len(df_annotated_data_list) != 0:
         df_from_list = pd.DataFrame(df_annotated_data_list, columns=ACCEPTED_COLUMNS)
