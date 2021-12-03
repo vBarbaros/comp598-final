085c74150a10a9e7b0bf9bc05ad95c671c64b252
vBarbaros <victor.barbarosh@gmail.com>
2021-12-02 23:02:57 -0500

    Annotate 200 from keywords (nov 29th batch)

--- src/twitter_annotation_handler.py
+++ src/twitter_annotation_handler.py
@@ -5,14 +5,16 @@ import pandas as pd
 
 from twitter_collect import get_file_paths, read_from_csv_collected_tweets, append_to_csv
 
-SAMPLE_SIZE = 200
 ACCEPTED_COLUMNS = ['text', 'label', 'sentiment', 'ca_related']
 USE_FILTER = 3
-OUT_FILE_JSON, OUT_FILE_CSV, OUT_FILE_FILTERED_CSV, OUT_FILE_FILTERED_JSON, SEARCH_QUERY = get_file_paths(USE_FILTER)
 
 parentdir = Path(__file__).parents[1]
 
-def main():
+def main(USE_FILTER, FILE_PREFIX):
+    global OUT_FILE_JSON, OUT_FILE_CSV, OUT_FILE_FILTERED_CSV, OUT_FILE_FILTERED_JSON, SEARCH_QUERY
+    OUT_FILE_JSON, OUT_FILE_CSV, OUT_FILE_FILTERED_CSV, OUT_FILE_FILTERED_JSON, SEARCH_QUERY = get_file_paths(
+        USE_FILTER, FILE_PREFIX)
+
     file_name = os.path.split(OUT_FILE_FILTERED_CSV)[-1].split('.')[0]
     annotated_file_name = 'data/annotated/' + file_name + '.csv'
     annotated_done_file_name = 'data/annotated/done/' + file_name + '.csv'
@@ -55,8 +57,6 @@ def main():
             print('\n=====================================', '\n=====================================')
 
             df_annotated_data_list.append((row['text'], label, sentiment, ca_related))
-        # if i == 5:
-        #     break
 
     if len(df_annotated_data_list) != 0:
         df_from_list = pd.DataFrame(df_annotated_data_list, columns=ACCEPTED_COLUMNS)
@@ -65,4 +65,17 @@ def main():
         append_to_csv(df_annotated_so_far, annotated_done_file_name)
 
 if __name__ == '__main__':
-    main()
+    # November 29 collected - no prefix
+    main(1, '')
+    # main(2, '')
+    # main(3, '')
+
+    # for i in [(1, 'key words channel')]:
+    # for i in [(2, 'news_cannels'), (3, 'official organisations channel')]:
+    #     print(f"\nUsing filter type - {i}")
+    #     main(i[0], 'nov_30_')
+
+    # for i in [(1, 'key words channel')]:
+    # for i in [(2, 'news_cannels'), (3, 'official organisations channel')]:
+    #     print(f"\nUsing filter type [{i}]")
+    #     main(i[0], 'dec_01_')
